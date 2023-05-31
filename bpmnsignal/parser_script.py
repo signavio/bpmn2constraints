ce70e2e81626d657464fe6f0f85c6ab22fe3de38
Arvid Bergman Thörn <arvid.bergman@sap.com>
2023-05-31 14:13:04 +0200

    removed old scripts (#54)

--- bpmnsignal/parser_script.py
+++ bpmnsignal/parser_script.py
@@ -1,97 +1,90 @@
 
-# pylint: disable=bare-except
 from tqdm import tqdm
-import traceback
 
 from bpmnsignal.parser.bpmn_parser import Parser
 from bpmnsignal.utils.script_utils import Setup
 from bpmnsignal.utils.plot import Plot
 
+FAIL = "failed"
+SUCCESS = "successful"
 
-def create_scatter_object(model_outcome, element_count, type_count):
-    return {
-        "outcome": model_outcome,
-        "number of elements": element_count,
-        "number of element types": type_count
-    }
+class ParserScript():
+    def __init__(self, path) -> None:
+        self.path = path
+        self.setup = Setup(path)
+        self.plot = Plot()
+        self.failed_models = 0
+        self.successful_models = 0
+        self.total_models = 0
+        self.total_elements = 0
+        self.total_parsed_elements = 0
+        self.parsed_models = []
 
-
-def create_partial_object(percentage, element_count, type_count):
-    return {
-        "variable": percentage,
-        "number of elements": element_count,
-        "number of element types": type_count
-    }
-
-
-def run_script(dir_path):
-    setup = Setup(dir_path)
-    plot = Plot()
-
-    failed_models = 0
-    successful_models = 0
-
-    total_parsed_elements = 0
-    total_elements = 0
-    total_models = 0
-
-    all_models = []
-
-    for filename in tqdm(setup.get_files()):
-        csv_file = setup.get_file(filename)
-
-        if setup.is_file(csv_file):
-            for chunk in setup.read_csv_chunk(csv_file):
-                models = setup.load_models(chunk)
-
-                for model in models:
-                    try:
-                        parser = Parser(model, False, False)
-
-                        # Check for pools before parser flattens diagram.
-                        if parser.count_pools() > 1:
-                            continue
-
-                        result = parser.run()
-
-                        # Check for starts after parser has flattened diagram.
-                        if not parser.has_start():
-                            continue
-                        # Check for elements after parser has flattened diagram.
-                        if parser.count_model_elements() < 5:
-                            continue
-
-                        model_elements = parser.count_model_elements()
-                        model_element_types = parser.count_model_element_types()
-
-                        total_models += 1
-                        parsed = len(result)
-                        parsable = parser.count_parsable_elements()
-
-                        total_parsed_elements += parsed
-                        total_elements += parsable
-
-                        if parsed == parsable:
-                            # Success
-                            successful_models += 1
-                            all_models.append(create_scatter_object(
-                                "successful", model_elements, model_element_types))
-
-                    except Exception:
-                        # Failed
-                        failed_models += 1
-                        # For some reason, I have to subtract here..
-                        successful_models -= 1
-                        all_models.append(create_scatter_object(
-                            "failed", model_elements, model_element_types))
-
-    plot.scatter_plot_model_outcomes(all_models, "All Parsed Models")
-    plot.bar_plot_model_outcomes(all_models)
-
-    print(f"Successful: {successful_models} out of {total_models}")
-    print(f"Failed: {failed_models} out of {total_models}")
-    print(f"Parsed {total_parsed_elements} out of {total_elements} elements.")
+    def create_scatter_object(self, model_outcome, element_count, type_count):
+        return {
+            "outcome": model_outcome,
+            "number of elements": element_count,
+            "number of element types": type_count
+        }
 
 
-if __name__ == "__main__":
-    run_script("../../Downloads/sap_sam_2022/models")
+    def create_partial_object(self, percentage, element_count, type_count):
+        return {
+            "variable": percentage,
+            "number of elements": element_count,
+            "number of element types": type_count
+        }
+
+    def run(self):
+        for filename in tqdm(self.setup.get_files()):
+            csv_file = self.setup.get_file(filename)
+
+            if self.setup.is_file(csv_file):
+                for chunk in self.setup.read_csv_chunk(csv_file):
+                    models = self.setup.load_models(chunk)
+
+                    for model in models:
+                        try:
+                            parser = Parser(model, False, False)
+
+                            if parser.count_pools() > 1:
+                                continue
+
+                            result = parser.run()
+
+                            if not parser.has_start():
+                                continue
+
+                            if parser.count_model_elements() < 5:
+                                continue
+
+                            model_elements = parser.count_model_elements()
+                            model_element_types = parser.count_model_element_types()
+
+                            self.total_models += 1
+                            parsed = len(result)
+                            parsable = parser.count_parsable_elements()
+
+                            self.total_parsed_elements += parsed
+                            self.total_elements += parsable
+
+                            if parsed == parsable:
+                                # Success
+                                self.successful_models += 1
+                                self.parsed_models.append(self.create_scatter_object(
+                                    SUCCESS, model_elements, model_element_types))
+
+                        except Exception:
+                            # Failed
+                            self.failed_models += 1
+                            # For some reason, I have to subtract here..
+                            self.successful_models -= 1
+                            self.parsed_models.append(self.create_scatter_object(
+                                FAIL, model_elements, model_element_types))
+
+        self.plot.scatter_plot_model_outcomes(self.parsed_models, "All Parsed Models")
+        self.plot.bar_plot_model_outcomes(self.parsed_models)
+
+        print(f"Successful: {self.successful_models} out of {self.total_models}")
+        print(f"Failed: {self.failed_models} out of {self.total_models}")
+        print(f"Parsed {self.total_parsed_elements} out of {self.total_elements} elements.")
--- bpmnsignal/script.py
+++ bpmnsignal/script.py
@@ -7,6 +7,8 @@ from json import dumps
 from bpmnsignal.parser.bpmn_parser import Parser
 from bpmnsignal.compiler.bpmn_compiler import Compiler
 from bpmnsignal.utils.script_utils import Setup
+from bpmnsignal.compiler_script import CompilerScript
+from bpmnsignal.parser_script import ParserScript
 
 
 
@@ -16,20 +18,15 @@ def run():
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--parse', type=str, help='Runs the parser')
-    parser.add_argument('--compile', type=str, help='Runs the compiler')
-    parser.add_argument('--transitivity',
-                        type=bool,
-                        help='Adds transitive constraints to compiler')
+    parser.add_argument("--parse", type=str, help="Runs the parser")
+    parser.add_argument("--compile", type=str, help="Runs the compiler")
+    parser.add_argument("--transitivity", type=bool, help="Adds transitivity")
+    parser.add_argument("--compare_constraints")
+    parser.add_argument("--dataset", type=str, help="Path to dataset to compile")
+    parser.add_argument("--dataframe", type=str, help="Path to dataframe of compiled constraints")
+    parser.add_argument("--parse_dataset", type=str, help="Path to dataset folder")
 
     args = parser.parse_args()
-
-    if args.parse:
-        path = Path(args.parse)
-        setup = Setup(None)
-        if setup.is_file(path):
-            res = Parser(path, True, args.transitivity).run()
-            print(dumps(res, indent=2))
 
     if args.parse:
         path = Path(args.parse)
@@ -45,6 +42,33 @@ def run():
             res = Parser(path, True, args.transitivity).run()
             res = Compiler(res, args.transitivity).run()
             print(dumps(res, indent=2))
+    
+    elif args.compare_constraints:
+        dataframe_path = None
+        dataset_path = None
+        setup = Setup(None)
 
+        if args.dataframe:
+            dataframe_path = Path(args.dataframe)
+        if args.dataset:
+            dataset_path = Path(args.dataset)
+
+        if dataframe_path is None or dataset_path is None:
+            return
+
+        if setup.is_file(dataframe_path) and setup.is_file(dataset_path):
+            script = CompilerScript(dataset_path, dataframe_path)
+            script.run()
+    
+    elif args.parse_dataset:
+        dataset_path = Path(args.parse_dataset)
+        setup = Setup(None)
+
+        if dataset_path is None:
+            return
+        
+        if setup.is_directory(dataset_path):
+            script = ParserScript(dataset_path)
+            script.run()
     else:
         parser.print_help()

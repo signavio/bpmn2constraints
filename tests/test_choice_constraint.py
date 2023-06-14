7d145c91317559f0529cf470cbd4de817cc9a6b9
arvidbt <arvid.bergman@sap.com>
2023-06-14 17:29:22 +0200

    added sequential flow tests + choice constraint test

--- tests/test_choice_constraint.py
+++
@@ -1,5 +0,0 @@
-def test_choice_constraint_is_not_generated_for_elements_in_gateway_constructs():
-    pass
-
-def test_choice_constraints_generated_for_sequential_flow():
-    pass
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from bpmnconstraints.parser.bpmn_parser import Parser
+from bpmnconstraints.compiler.bpmn_compiler import Compiler
 from bpmnconstraints.utils.script_utils import Setup
 
 def init_test_setup_for_parser(diagram_constant):
@@ -8,3 +9,14 @@ def init_test_setup_for_parser(diagram_constant):
     if setup.is_file(path):
         res = Parser(path, True, True).run()
         return res
+    
+def init_test_setup_for_compiler(diagram_constant):
+    path = Path(diagram_constant.get("path"))
+    constraints = []
+    setup = Setup(None)
+    if setup.is_file(path):
+        res = Parser(path, True, False).run()
+        res = Compiler(res, True).run()
+        for con in res:
+            constraints.append(con.get("DECLARE"))
+        return constraints

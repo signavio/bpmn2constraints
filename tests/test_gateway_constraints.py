6cb344f2e015220419a064f3b57381b744cb33ff
arvidbt <arvid.bergman@sap.com>
2023-06-14 18:01:37 +0200

    added test for gateway combinations

---
+++ tests/test_gateway_constraints.py
@@ -0,0 +1,11 @@
+from test_utils import init_test_setup_for_compiler
+from file_constants import THREE_SPLIT_XOR_GATEWAY_DIAGRAM
+
+def test_that_all_gateway_constraints_are_generated():
+    res = init_test_setup_for_compiler(THREE_SPLIT_XOR_GATEWAY_DIAGRAM)
+    expected_gateway_constraints = [
+        "Exclusive Choice[activity four, activity two]",
+        "Exclusive Choice[activity three, activity two]",
+        "Exclusive Choice[activity three, activity four]",
+    ]
+    assert all(constraint in res for constraint in expected_gateway_constraints)

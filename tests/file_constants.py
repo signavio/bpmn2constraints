519b34df02200a9666f97939725bb2ff5780641a
arvidbt <arvid.bergman@sap.com>
2023-06-14 12:33:16 +0200

    added new diagram for multiple starts

--- tests/file_constants.py
+++ tests/file_constants.py
@@ -10,3 +10,10 @@ LINEAR_SEQUENCE_DIAGRAM_WITHOUT_START_AND_END = {
     "start element id" : "sid-E9508543-5660-4C85-9E42-1119DAAD80C2",
     "end element id" : "sid-08543ADB-C8D6-4026-8A03-1228CC559A7F", 
 }
+
+MULTIPLE_STARTS_DIAGRAM = {
+    "path" : "examples/misc/multiple_starts.json",
+    "start element 1 id" : "sid-63552E64-B2A2-4D39-A168-2C6A30BB76F7",
+    "start element 2 id" : "sid-AB69BFC4-A028-480F-BC16-BAC74F4A8EDD",
+    "number of starts" : 2,
+}

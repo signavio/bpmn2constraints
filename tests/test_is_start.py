b1013fe5023596d65da3e5e3559c8b3da91a67b0
arvidbt <arvid.bergman@sap.com>
2023-06-14 12:24:46 +0200

    2/3 is start tests

--- tests/test_is_start.py
+++ tests/test_is_start.py
@@ -2,20 +2,30 @@ from pathlib import Path
 from bpmnconstraints.parser.bpmn_parser import Parser
 from bpmnconstraints.utils.script_utils import Setup
 
-from file_constants import LINEAR_SEQUENCE_DIAGRAM_WITH_START_AND_END, LINEAR_SEQUENCE_DIAGRAM_WITHOUT_START_AND_END
+from file_constants import (
+    LINEAR_SEQUENCE_DIAGRAM_WITH_START_AND_END,
+    LINEAR_SEQUENCE_DIAGRAM_WITHOUT_START_AND_END,
+)
 
-def test_element_is_marked_as_start_if_it_has_no_predecessors():
-    path = Path(LINEAR_SEQUENCE_DIAGRAM_WITHOUT_START_AND_END.get("path"))
+def init_test_setup(diagram_constant):
+    path = Path(diagram_constant.get("path"))
     setup = Setup(None)
     if setup.is_file(path):
         res = Parser(path, True, False).run()
+        return res
+    
+def test_element_is_marked_as_start_if_it_has_no_predecessors():
+    res = init_test_setup(LINEAR_SEQUENCE_DIAGRAM_WITHOUT_START_AND_END)
+    for elem in res:
+        if elem.get("is start"):
+            assert elem.get("id") == LINEAR_SEQUENCE_DIAGRAM_WITHOUT_START_AND_END.get("start element id")
 
 def test_element_is_marked_as_start_if_predecessor_is_start_event():
-    path = Path(LINEAR_SEQUENCE_DIAGRAM_WITH_START_AND_END.get("path"))
-    setup = Setup(None)
-    if setup.is_file(path):
-        res = Parser(path, True, False).run()
-        
+    res = init_test_setup(LINEAR_SEQUENCE_DIAGRAM_WITH_START_AND_END)
+    for elem in res:
+        if elem.get("is start"):
+            assert elem.get("id") == LINEAR_SEQUENCE_DIAGRAM_WITH_START_AND_END.get("start element id")
+
 
 def test_all_start_elements_are_marked_as_starts_if_multiple_starts():
     pass

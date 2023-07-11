b12728abc1f6f1fdec2ec2508718a32e820beee9
arvidbt <arvid.bergman@sap.com>
2023-07-11 13:31:54 +0200

    format

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 """Setup for running the bpmnconstraints script."""
 import setuptools
 
-with open('README.md', encoding='utf-8') as file:
+with open("README.md", encoding="utf-8") as file:
     long_description = file.read().strip()
 
 setuptools.setup(
-    name='bpmnconstraints',
-    version='0.0.2',
-    description='Compiles BPMN models to SIGNAL queries and LTL constraints',
+    name="bpmnconstraints",
+    version="0.0.2",
+    description="Compiles BPMN models to SIGNAL queries and LTL constraints",
     long_description=long_description,
-    author='Arvid Bergman, Timotheus Kampik, Adrian Rebmann',
-    author_email='timotheus.kampik@sap.com',
-    url='https://github.com/signavio/bpmn2constraints',
-    py_modules=['bpmnconstraints'],
-    entry_points={'console_scripts': ['bpmnconstraints=bpmnconstraints.script:run']},
-    install_requires=['pylogics', 'xmltodict'],
-    keywords='BPMN Conformance',
-    classifiers=['BPMN', 'Conformance Checking'])
+    author="Arvid Bergman, Timotheus Kampik, Adrian Rebmann",
+    author_email="timotheus.kampik@sap.com",
+    url="https://github.com/signavio/bpmn2constraints",
+    py_modules=["bpmnconstraints"],
+    entry_points={"console_scripts": ["bpmnconstraints=bpmnconstraints.script:run"]},
+    install_requires=["pylogics", "xmltodict"],
+    keywords="BPMN Conformance",
+    classifiers=["BPMN", "Conformance Checking"],
+)
--- tutorial/SignavioAuthenticator.py
+++ tutorial/SignavioAuthenticator.py
@@ -1,7 +1,7 @@
 import requests
 
-class SignavioAuthenticator:
 
+class SignavioAuthenticator:
     def __init__(self, system_instance, tenant_id, email, pw):
         self.system_instance = system_instance
         self.tenant_id = tenant_id
@@ -18,29 +18,25 @@ class SignavioAuthenticator:
         Returns:
             dictionary: Session information
         """
-        login_url = self.system_instance + '/p/login'
-        data = {
-            'name': self.email,
-            'password': self.pw,
-            'tokenonly': 'true'
-        }
-        if 'tenant_id' in locals():
-            data['tenant'] = self.tenant_id
+        login_url = self.system_instance + "/p/login"
+        data = {"name": self.email, "password": self.pw, "tokenonly": "true"}
+        if "tenant_id" in locals():
+            data["tenant"] = self.tenant_id
         # authenticate
         login_request = requests.post(login_url, data)
 
         # retrieve token and session ID
-        auth_token = login_request.content.decode('utf-8')
-        jsesssion_ID = login_request.cookies['JSESSIONID']
+        auth_token = login_request.content.decode("utf-8")
+        jsesssion_ID = login_request.cookies["JSESSIONID"]
 
         # The cookie is named 'LBROUTEID' for base_url 'editor.signavio.com'
         # and 'editor.signavio.com', and 'AWSELB' for base_url
         # 'app-au.signavio.com' and 'app-us.signavio.com'
-        lb_route_ID = login_request.cookies['LBROUTEID']
+        lb_route_ID = login_request.cookies["LBROUTEID"]
 
         # return credentials
         return {
-            'jsesssion_ID': jsesssion_ID,
-            'lb_route_ID': lb_route_ID,
-            'auth_token': auth_token
+            "jsesssion_ID": jsesssion_ID,
+            "lb_route_ID": lb_route_ID,
+            "auth_token": auth_token,
         }

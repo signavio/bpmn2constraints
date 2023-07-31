5b2ace6ff315e686ab134b02d8856c761ff4a24c
arvidbt <arvid.bergman@sap.com>
2023-07-31 14:52:13 +0200

    refactor

--- bpmnconstraints/mermaid/mermaidtranslation.py
+++ bpmnconstraints/mermaid/mermaidtranslation.py
@@ -5,67 +5,44 @@ SEQUENCE_FLOW = "-->"
 class Mermaid:
     def __init__(self, bpmn) -> None:
         self.bpmn = bpmn
-
-    def __gen_flowchart(self):
-        pass
+        self.ids = {}
 
     def translate(self):
         rows = []
         for elem in self.bpmn:
             if elem["is start"]:
-                rows.extend(self.__create_start_node(elem))
-            
+                rows.extend(self.__create_node(elem, self.__gen_event_str))
+
             elif elem["type"] in ALLOWED_ACTIVITIES and not elem["is end"]:
-                rows.extend(self.__create_activity_node(elem))
+                rows.extend(self.__create_node(elem, self.__gen_activity_str))
 
             elif elem["type"] in ALLOWED_GATEWAYS:
-                rows.extend(self.__create_gateway_node(elem))
+                rows.extend(self.__create_node(elem, self.__gen_gateway_str))
 
         return self.__gen_flowchart(rows)
 
-    def __create_gateway_node(self, elem):
-        rows = []
-        for successor in elem["successor"]:
-            row = ""
-            row += self.__gen_gateway_str(elem["id"], elem["name"])
-            row += SEQUENCE_FLOW
-            if successor["is end"]:
-                row += self.__gen_event_str(successor["id"], successor["name"])
-            elif successor["type"] in ALLOWED_ACTIVITIES:
-                row += self.__gen_activity_str(successor["id"], successor["name"])
-            elif successor["type"] in ALLOWED_GATEWAYS:
-                row += self.__gen_gateway_str(successor["id"], successor["name"])
-            rows.append(row)
-        return rows
-    
-    def __create_activity_node(self, elem):
-        rows = []
-        for successor in elem["successor"]:
-            row = ""
-            row += self.__gen_activity_str(elem["id"], elem["name"])
-            row += SEQUENCE_FLOW
+    def __match_successor_str(self, successor):
+        successor_id = self.__gen_new_id(successor["id"])
+        if successor["is end"]:
+            return self.__gen_event_str(successor_id, successor["name"])
+        elif successor["type"] in ALLOWED_ACTIVITIES:
+            return self.__gen_activity_str(successor_id, successor["name"])
+        elif successor["type"] in ALLOWED_GATEWAYS:
+            return self.__gen_gateway_str(successor_id, successor["name"])
 
-            if successor["is end"]:
-                row += self.__gen_event_str(successor["id"], successor["name"])
-            elif successor["type"] in ALLOWED_ACTIVITIES:
-                row += self.__gen_activity_str(successor["id"], successor["name"])
-            elif successor["type"] in ALLOWED_GATEWAYS:
-                row += self.__gen_gateway_str(successor["id"], successor["name"])
-            rows.append(row)
-        return rows
-    
-    def __create_start_node(self, elem):
+    def __create_node(self, elem, gen_str_func):
         rows = []
-        for succcessor in elem["successor"]:
-            row = ""
-            row += self.__gen_event_str(elem["id"], elem["name"])
-            row += SEQUENCE_FLOW
-            row += self.__gen_activity_str(succcessor["id"], succcessor["name"])
+        for successor in elem["successor"]:
+            node_id = self.__gen_new_id(elem["id"])
+            row = f"{gen_str_func(node_id, elem['name'])}{SEQUENCE_FLOW}{self.__match_successor_str(successor)}"
             rows.append(row)
         return rows
-    
-    def __gen_new_id(self):
-        pass
+
+    def __gen_new_id(self, old_id):
+        if old_id not in self.ids:
+            index = len(self.ids)
+            self.ids[old_id] = "ID" + str(index)
+        return self.ids[old_id]
 
     def __gen_flowchart(self, rows):
         flowchart = "flowchart TD\n"
@@ -73,12 +50,11 @@ class Mermaid:
             flowchart += f"    {row}\n"
         return flowchart
 
-    def __gen_event_str(self, id, msg):
-        return f"{id}(({msg}))"
+    def __gen_event_str(self, node_id, msg):
+        return f"{node_id}(({msg}))"
 
-    def __gen_gateway_str(self, id, msg):
-        # Can't use formatted string because of the { } required by mermaid.
-        return id + "{" + msg + "}"
+    def __gen_gateway_str(self, node_id, msg):
+        return "{}{{{}}}".format(node_id, msg)
 
-    def __gen_activity_str(self, id, msg):
-        return f"{id}({msg})"
+    def __gen_activity_str(self, node_id, msg):
+        return f"{node_id}({msg})"

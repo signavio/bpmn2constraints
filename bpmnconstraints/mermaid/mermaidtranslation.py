51d18a10ee752dfdc708b1d1146fac4bb3b5ad93
arvidbt <arvid.bergman@sap.com>
2023-07-31 15:02:51 +0200

    fix missing name, use type instead

--- bpmnconstraints/mermaid/mermaidtranslation.py
+++ bpmnconstraints/mermaid/mermaidtranslation.py
@@ -21,27 +21,35 @@ class Mermaid:
 
         return self.__gen_flowchart(rows)
 
+    def __get_node_name(self, elem):
+        successor_name = elem["name"]
+        if successor_name == "":
+            successor_name = elem["type"]
+        return successor_name
+
     def __match_successor_str(self, successor):
         successor_id = self.__gen_new_id(successor["id"])
+        successor_name = self.__get_node_name(successor)
         if successor["is end"]:
-            return self.__gen_event_str(successor_id, successor["name"])
+            return self.__gen_event_str(successor_id, successor_name)
         elif successor["type"] in ALLOWED_ACTIVITIES:
-            return self.__gen_activity_str(successor_id, successor["name"])
+            return self.__gen_activity_str(successor_id, successor_name)
         elif successor["type"] in ALLOWED_GATEWAYS:
-            return self.__gen_gateway_str(successor_id, successor["name"])
+            return self.__gen_gateway_str(successor_id, successor_name)
 
     def __create_node(self, elem, gen_str_func):
         rows = []
         for successor in elem["successor"]:
             node_id = self.__gen_new_id(elem["id"])
-            row = f"{gen_str_func(node_id, elem['name'])}{SEQUENCE_FLOW}{self.__match_successor_str(successor)}"
+            node_name = self.__get_node_name(elem)
+            row = f"{gen_str_func(node_id, node_name)}{SEQUENCE_FLOW}{self.__match_successor_str(successor)}"
             rows.append(row)
         return rows
 
     def __gen_new_id(self, old_id):
         if old_id not in self.ids:
             index = len(self.ids)
-            self.ids[old_id] = "ID" + str(index)
+            self.ids[old_id] = "id" + str(index)
         return self.ids[old_id]
 
     def __gen_flowchart(self, rows):

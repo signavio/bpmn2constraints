ac3a459651bae85f0a5369f205d1949e52a87247
arvidbt <arvid.bergman@sap.com>
2023-08-02 12:38:51 +0200

    fix lint...

--- tests/test_mermaid.py
+++ tests/test_mermaid.py
@@ -1,9 +1,11 @@
 from file_constants import LINEAR_MERMAID_GRAPH, GATEWAY_MERMAID_GRAPH
 from test_utils import init_test_setupt_for_mermaid
 
+
 def test_linear_mermaid_graph():
     res = init_test_setupt_for_mermaid(LINEAR_MERMAID_GRAPH)
     assert LINEAR_MERMAID_GRAPH.get("output") == res
+
 
 def test_gateway_mermaid_graph():
     res = init_test_setupt_for_mermaid(GATEWAY_MERMAID_GRAPH)

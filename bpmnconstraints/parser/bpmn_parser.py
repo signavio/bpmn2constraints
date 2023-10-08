265e017c8eba7a09252ca10f2d322a93c5245487
MarcusRostSAP <marcus.rost@hotmail.com>
2023-10-08 14:19:20 +0200

    formatting

--- bpmnconstraints/parser/bpmn_parser.py
+++ bpmnconstraints/parser/bpmn_parser.py
@@ -68,7 +68,6 @@ class Parser:
 
     def validate_edge_cases(self):
         item_indices = {item["name"]: index for index, item in enumerate(self.sequence)}
-        
         for cfo in self.sequence:
             if cfo["is start"] and cfo["name"] == "XOR":
                 cfo["is start"] = False

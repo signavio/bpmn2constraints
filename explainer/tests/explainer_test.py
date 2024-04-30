f67caf376a269ada25ef616b58dd0e8728d80de8
MarcusRostSAP <marcus.rost@sap.com>
2024-04-30 10:53:13 +0200

    Changed location of tests

--- explainer/tests/explainer_test.py
+++ explainer/tests/explainer_test.py
@@ -1,7 +1,5 @@
-import sys
-sys.path.append('../../')
 from explainer.explainer import *
-from explainer import ExplainerRegex
+from explainer.explainer_regex import ExplainerRegex
 
 # Test 1: Adding and checking constraints
 def test_add_constraint():
